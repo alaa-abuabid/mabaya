fe6ae28f9947e3f2459f573185fe0669aa666aa4
Alaa Abu Abid <alaa.abuabid@gmail.com>
2019-11-04 11:20:43 +0200

    repositories

---
+++ src/main/java/com/alaa/mabaya/repository/CampaignRespository.java
@@ -0,0 +1,9 @@
+package com.alaa.mabaya.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alaa.mabaya.model.Campaign;
+
+public interface CampaignRespository extends JpaRepository<Campaign, Integer>{
+
+}
